0a901ae4cc235bbaff4f4b172ecd714f159eda44
jkmd <jan-karl@jan-karl.com>
2015-01-10 14:55:07 -0500

    teachers page looks pretty good on mobile, nav works

--- js/mockup1.js
+++ js/mockup1.js
@@ -1,3 +1,17 @@
 function toggleNav(){
 	$('#navbar #navbuttons').toggle();
 }
+
+function showPage(page){
+	$('.page').hide();
+	$('#'+page).show();
+}
+
+function init(){
+	$('#navbar #navbuttons li a').click(function(){
+		var page = $(this).text().replace(' ','').toLowerCase();
+		showPage(page);
+	})
+}
+
+$(document).ready(init);
