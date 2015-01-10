7684eb84e1f37d53ae1d3dc92c8638e2e7ea6122
jkmd <jan-karl@jan-karl.com>
2015-01-10 15:20:54 -0500

    stuf fworking better

--- js/mockup1.js
+++ js/mockup1.js
@@ -1,15 +1,18 @@
 function toggleNav(){
-	$('#navbar #navbuttons').toggle();
+	$('#navbar #navbuttons').toggleClass('selected');
 }
 
 function showPage(page){
 	$('.page').hide();
 	$('#'+page).show();
+	$('#navbar a').removeClass('selected');
+	$('#navbar #nav_'+page).addClass('selected');
+	$('#navbar #navbuttons').removeClass('selected');
 }
 
 function init(){
 	$('#navbar #navbuttons li a').click(function(){
-		var page = $(this).text().replace(' ','').toLowerCase();
+		var page = $(this).attr('id').replace('nav_','');
 		showPage(page);
 	})
 }
