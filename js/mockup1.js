2febe9e86c3c1fdbd3d26d584d5bd1ed2b009104
jkmd <jan-karl@jan-karl.com>
2015-01-23 18:28:51 -0500

    this iteration is not very good

--- js/mockup1.js
+++ js/mockup1.js
@@ -10,11 +10,30 @@ function showPage(page){
 	$('#navbar #navbuttons').removeClass('selected');
 }
 
+function showTeacher(teacher){
+	$('#teachers .info').removeClass('selected');
+	$('#teachers #' + teacher).addClass('selected');
+}
+
+
 function init(){
 	$('#navbar #navbuttons li a').click(function(){
 		var page = $(this).attr('id').replace('nav_','');
 		showPage(page);
+	});
+
+	$('#teachers img, #teachers h1').click(function(){
+		var teacher = $(this).parent('.info').attr('id');
+		showTeacher(teacher);
+	});
+
+
+	$('#teachers .nav a').click(function(){
+		var teacher = $(this).text();
+		showTeacher(teacher);
 	})
+
+
 }
 
 $(document).ready(init);
